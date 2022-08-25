9c26a17d4e7fa4bec57feaa66673bd30ddc3949e
monneratachiame <monneratachiame@gmail.com>
2022-08-25 14:22:17 +0000

    project finished

--- src/app.js
+++ src/app.js
@@ -13,8 +13,7 @@ let generateExcuse = () => {
   let what = ["my homework", "the keys", "the car"];
   let when = [
     "before the class",
-    "right on time", "when I finished", "during my lunch",
-    "while I was praying"
+    "right on time", "when I finished", "during my lunch", "while I was praying"
   ];
 
   let whoIndex = Math.floor(Math.random() * who.length);
